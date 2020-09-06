97bce52089771e985eb4f2012173b2c7f36b60ea
Diego Varela <diegocrash124@gmail.com>
2020-09-05 22:38:19 -0600

    Se hacen los movimientos de alfil y caballo

--- Alfil.hpp
+++ Alfil.hpp
@@ -1,10 +1,235 @@
 #include "Pieza.hpp"
 #ifndef ALFIL_H
 #define ALFIL_H
-class Alfil:public Pieza{
-    public:
-    virtual bool validarmovimiento(string pieza,int x,int y,int x2,int y2,string** tablero){
-        
+class Alfil : public Pieza
+{
+public:
+    bool validarmovimiento(string pieza, int x, int y, int x2, int y2, string **tablero)
+    {
+        if (pieza == "B")
+        {
+            if ((x != x2) && (y != y2))
+            {
+                //For moverse diagonal para arriba a la derecha
+                int contdiagonalder = 1;
+                int contdiagonaliz = 1;
+                int contdiagonalderaba = 1;
+                int contdiagonalizaba = 1;
+                for (int i = y; i < 8; i++)
+                    for (int i = y; i < 8; i++)
+                    {
+                        if (tablero[x - contdiagonalder][y + contdiagonalder] == "P" || tablero[x - contdiagonalder][y + contdiagonalder] == "B" || tablero[x - contdiagonalder][y + contdiagonalder] == "R" || tablero[x - contdiagonalder][y + contdiagonalder] == "Q" || tablero[x - contdiagonalder][y + contdiagonalder] == "K" || tablero[x - contdiagonalder][y + contdiagonalder] == "N")
+                        {
+                            if (tablero[x - contdiagonaliz][y - contdiagonaliz] == "P" || tablero[x - contdiagonaliz][y - contdiagonaliz] == "B" || tablero[x - contdiagonaliz][y - contdiagonaliz] == "R" || tablero[x - contdiagonaliz][y - contdiagonaliz] == "Q" || tablero[x - contdiagonaliz][y - contdiagonaliz] == "N" || tablero[x - contdiagonaliz][y - contdiagonaliz] == "K")
+                            {
+                                if (tablero[x + contdiagonalderaba][y + contdiagonalderaba] == "P" || tablero[x + contdiagonalderaba][y + contdiagonalderaba] == "R" || tablero[x + contdiagonalderaba][y + contdiagonalderaba] == "B" || tablero[x + contdiagonalderaba][y + contdiagonalderaba] == "N" || tablero[x + contdiagonalderaba][y + contdiagonalderaba] == "Q" || tablero[x + contdiagonalderaba][y + contdiagonalderaba] == "K")
+                                {
+                                    if (tablero[x + contdiagonalizaba][y - contdiagonalizaba] == "P" || tablero[x + contdiagonalizaba][y - contdiagonalizaba] == "R" || tablero[x + contdiagonalizaba][y - contdiagonalizaba] == "B" || tablero[x + contdiagonalizaba][y - contdiagonalizaba] == "N" || tablero[x + contdiagonalizaba][y - contdiagonalizaba] == "Q" || tablero[x + contdiagonalizaba][y - contdiagonalizaba] == "K")
+                                    {
+                                        return false;
+                                    }
+                                    else
+                                    {
+                                        if (tablero[x2-1][y2+1] == "p" ||tablero[x2-1][y2+1]== "r" || tablero[x2-1][y2+1] == "b" || tablero[x2-1][y2+1] == "n" || tablero[x2-1][y2+1] == "q" || tablero[x2-1][y2+1] == "k")
+                                        {
+                                            if (tablero[x2][y2] == " ")
+                                            {
+                                                return true;
+                                            }
+                                            else
+                                            {
+                                                return false;
+                                            }
+                                        }
+                                        else if ((x + contdiagonalizaba == x2) && (y - contdiagonalizaba == y2))
+                                        {
+                                            return true;
+                                        }
+                                        return true;
+                                    }
+                                }
+                                else
+                                {
+                                    if (tablero[x2-1][y2-1] == "p" || tablero[x2-1][y2-1] == "r" ||tablero[x2-1][y2-1]== "b" || tablero[x2-1][y2-1] == "n" || tablero[x2-1][y2-1] == "q" ||tablero[x2-1][y2-1] == "k")
+                                    {
+                                        if (tablero[x2][y2] == " ")
+                                        {
+                                            return true;
+                                        }
+                                        else
+                                        {
+                                            return false;
+                                        }
+                                    }
+                                    else if ((x + contdiagonalderaba == x2) && (y + contdiagonalderaba == y2))
+                                    {
+                                        return true;
+                                    }
+                                    return true;
+                                }
+                            }
+                            else
+                            {
+                                if (tablero[x2 -1][y2 +1] == "p" || tablero[x2 +1][y2 + 1] == "r" || tablero[x2 + 1][y2 + 1] == "b" || tablero[x2 + 1][y2+ 1] == "n" ||tablero[x2 + 1][y2 + 1]== "q" ||tablero[x2 + 1][y2 + 1] == "k")
+                                {
+                                    if (tablero[x2][y2] == " ")
+                                    {
+                                        return true;
+                                    }
+                                    else
+                                    {
+                                        return false;
+                                    }
+                                }
+                                else if ((x - contdiagonaliz == x2) && (y - contdiagonaliz == y2))
+                                {
+                                    return true;
+                                }
+                                return true;
+                            }
+                        }
+                        else
+                        {
+                            if (tablero[x2 + 1][y2 - 1] == "p" || tablero[x2 + 1][y2 - 1] == "r" || tablero[x2 + 1][y2 - 1] == "b" || tablero[x2 + 1][y2 - 1] == "n" || tablero[x2 + 1][y2 - 1] == "q" || tablero[x2 + 1][y2 - 1] == "k")
+                            {
+                                if (tablero[x2][y2] == " ")
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    return false;
+                                }
+                            }
+                            else if ((x - contdiagonalder == x2) && (y + contdiagonalder == y2))
+                            {
+                                return true;
+                            }
+                            return true;
+                        }
+                        contdiagonalder++;
+                        contdiagonaliz++;
+                        contdiagonalderaba++;
+                        contdiagonalizaba++;
+                    }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (pieza == "b")
+        {
+            if ((x != x2) && (y != y2))
+            {
+                //For moverse diagonal para arriba a la derecha
+                int contdiagonalder = 1;
+                int contdiagonaliz = 1;
+                int contdiagonalderaba = 1;
+                int contdiagonalizaba = 1;
+                for (int i = y; i < 8; i++)
+                {
+                    if (tablero[x + contdiagonalder][y + contdiagonalder] == "p" || tablero[x + contdiagonalder][y + contdiagonalder] == "b" || tablero[x + contdiagonalder][y + contdiagonalder] == "r" || tablero[x + -contdiagonalder][y + contdiagonalder] == "q" || tablero[x + contdiagonalder][y + contdiagonalder] == "k" || tablero[x + contdiagonalder][y + contdiagonalder] == "n")
+                    {
+                        if (tablero[x + contdiagonaliz][y - contdiagonaliz] == "p" || tablero[x + contdiagonaliz][y - contdiagonaliz] == "b" || tablero[x + contdiagonaliz][y - contdiagonaliz] == "b" || tablero[x + contdiagonaliz][y - contdiagonaliz] == "q" || tablero[x + contdiagonaliz][y - contdiagonaliz] == "n" || tablero[x + contdiagonaliz][y - contdiagonaliz] == "k")
+                        {
+                            if (tablero[x - contdiagonalderaba][y - contdiagonalderaba] == "p" || tablero[x - contdiagonalderaba][y + contdiagonalderaba] == "r" || tablero[x - contdiagonalderaba][y + contdiagonalderaba] == "b" || tablero[x - contdiagonalderaba][y + contdiagonalderaba] == "n" || tablero[x - contdiagonalderaba][y + contdiagonalderaba] == "q" || tablero[x - contdiagonalderaba][y + contdiagonalderaba] == "k")
+                            {
+                                if (tablero[x - contdiagonalizaba][y - contdiagonalizaba] == "p" || tablero[x - contdiagonalizaba][y - contdiagonalizaba] == "p" || tablero[x - contdiagonalizaba][y - contdiagonalizaba] == "b" || tablero[x - contdiagonalizaba][y - contdiagonalizaba] == "n" || tablero[x - contdiagonalizaba][y - contdiagonalizaba] == "q" || tablero[x - contdiagonalizaba][y - contdiagonalizaba] == "k")
+                                {
+                                    return false;
+                                }
+                                else
+                                {
+                                    if (tablero[x2 +1][y2-1] == "P" ||tablero[x2 +1][y2-1] == "R" || tablero[x2 +1][y2-1] == "B" || tablero[x2 +1][y2-1]== "N" || tablero[x2 +1][y2-1] == "Q" || tablero[x2 +1][y2-1] == "K")
+                                    {
+                                        if (tablero[x2][y2] == " ")
+                                        {
+                                            return true;
+                                        }
+                                        else
+                                        {
+                                            return false;
+                                        }
+                                    }
+                                    else if ((x - contdiagonalizaba == x2) && (y - contdiagonalizaba == y2))
+                                    {
+                                        return true;
+                                    }
+                                    return true;
+                                }
+                            }
+                            else
+                            {
+                                if (tablero[x2+1][y2+1] == "p" || tablero[x2+1][y2+1] == "R" ||tablero[x2+1][y2+1]  == "B" || tablero[x2+1][y2+1]  == "N" ||tablero[x2+1][y2+1]  == "Q" || tablero[x2+1][y2+1]  == "K")
+                                {
+                                    if (tablero[x2][y2] == " ")
+                                    {
+                                        return true;
+                                    }
+                                    else
+                                    {
+                                        return false;
+                                    }
+                                }
+                                else if ((x - contdiagonalderaba == x2) && (y + contdiagonalderaba == y2))
+                                {
+                                    return true;
+                                }
+                                return true;
+                            }
+                        }
+                        else
+                        {
+                            if (tablero[x2-1][y2-1] == "P" || tablero[x2-1][y2-1]== "R" ||tablero[x2-1][y2-1] == "B" || tablero[x2-1][y2-1]== "Q" || tablero[x2-1][y2-1] == "N" || tablero[x2-1][y2-1]== "K")
+                            {
+                                if (tablero[x2][y2] == " ")
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    return false;
+                                }
+                            }
+                            else if ((x + contdiagonaliz == x2) && (y - contdiagonaliz == y2))
+                            {
+                                return true;
+                            }
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        if (tablero[x2-1][y2+1] == "P" || tablero[x2-1][y2+1] == "R" || tablero[x2-1][y2+1]== "B" || tablero[x2-1][y2+1] == "N" || tablero[x2-1][y2+1] == "Q" ||tablero[x2-1][y2+1] == "K")
+                        {
+                            if (tablero[x2][y2] == " ")
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else if ((x + contdiagonalder == x2) && (y + contdiagonalder == y2))
+                        {
+                            return true;
+                        }
+                        return true;
+                    }
+                    contdiagonalder++;
+                    contdiagonaliz++;
+                    contdiagonalderaba++;
+                    contdiagonalizaba++;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 };
 #endif
--- Caballo.hpp
+++ Caballo.hpp
@@ -1,10 +1,181 @@
 #include "Pieza.hpp"
 #ifndef CABALLO_H
 #define CABALLO_H
-class Caballo:public Pieza{
-    public:
-    virtual bool validarmovimiento(string pieza,int x,int y,int x2,int y2,string** tablero){
-        
+class Caballo : public Pieza
+{
+public:
+    bool validarmovimiento(string pieza, int x, int y, int x2, int y2, string **tablero)
+    {
+        if (pieza == "N")
+        {
+            if (x != x2 && y != y2)
+            {
+                /* int contdiagonalder = 1;
+                for (int i = y; i < 8; i++)
+                {
+                    if ((x - contdiagonalder == x2) && (y + contdiagonalder == y2))
+                    {
+                        return false;
+                    }else{
+                        return true;
+                    }
+                    contdiagonalder++;
+                }
+                int contdiagonaliz = 1;
+                for (int i = y; i < 8; i++)
+                {
+                    if ((x - contdiagonaliz == x2) && (y - contdiagonaliz == y2))
+                    {
+                        return false;
+                    }
+                    contdiagonaliz++;
+                }
+                int contdiagonalderaba = 1;
+                for (int i = y; i < 8; i++)
+                {
+                    if ((x + contdiagonalderaba == x2) && (y + contdiagonalderaba == y2))
+                    {
+                        return false;
+                    }
+                    contdiagonalderaba++;
+                }
+                int contdiagonalizaba = 1;
+                for (int i = y; i < 8; i++)
+                {
+                    if ((x + contdiagonalizaba == x2) && (y - contdiagonalizaba == y2))
+                    {
+                        return false;
+                    }
+                    contdiagonalizaba++;
+                }*/
+                if (tablero[x - 2][y + 1] == "P" || tablero[x - 2][y + 1] == "R" || tablero[x - 2][y + 1] == "B" || tablero[x - 2][y + 1] == "N" || tablero[x - 2][y + 1] == "Q" || tablero[x - 2][y + 1] == "K")
+                {
+                    if (tablero[x - 1][y + 2] == "P" || tablero[x - 1][y + 2] == "R" || tablero[x - 1][y + 2] == "B" || tablero[x - 1][y + 2] == "N" || tablero[x - 1][y + 2] == "Q" || tablero[x - 1][y + 2] == "K")
+                    {
+                        if (tablero[x + 1][y + 2] == "P" || tablero[x + 1][y + 2] == "R" || tablero[x + 1][y + 2] == "B" || tablero[x + 1][y + 2] == "N" || tablero[x + 1][y + 2] == "Q" || tablero[x + 1][y + 2] == "K")
+                        {
+                            if (tablero[x + 2][y + 1] == "P" || tablero[x + 2][y + 1] == "R" || tablero[x + 2][y + 1] == "B" || tablero[x + 2][y + 1] == "N" || tablero[x + 2][y + 1] == "Q" || tablero[x + 2][y + 1] == "K")
+                            {
+                                if (tablero[x + 2][y - 1] == "P" || tablero[x + 2][y - 1] == "R" || tablero[x + 2][y - 1] == "B" || tablero[x + 2][y - 1] == "N" || tablero[x + 2][y - 1] == "Q" || tablero[x + 2][y - 1] == "K")
+                                {
+                                    if (tablero[x + 1][y - 2] == "P" || tablero[x + 1][y - 2] == "R" || tablero[x + 1][y - 2] == "B" || tablero[x + 1][y - 2] == "N" || tablero[x + 1][y - 2] == "Q" || tablero[x + 1][y - 2] == "K")
+                                    {
+                                        if (tablero[x - 1][y - 2] == "P" || tablero[x - 1][y - 2] == "R" || tablero[x - 1][y - 2] == "B" || tablero[x - 1][y - 2] == "N" || tablero[x - 1][y - 2] == "Q" || tablero[x - 1][y - 2] == "K")
+                                        {
+                                            if (tablero[x - 2][y - 1] == "P" || tablero[x - 2][y - 1] == "R" || tablero[x - 2][y - 1] == "B" || tablero[x - 2][y - 1] == "N" || tablero[x - 2][y - 1] == "Q" || tablero[x - 2][y + 1] == "K")
+                                            {
+                                                return false;
+                                            }
+                                            else
+                                            {
+
+                                                return true;
+                                            }
+                                        }
+                                        else
+                                        {
+
+                                            return true;
+                                        }
+                                    }
+                                    else
+                                    {
+
+                                        return true;
+                                    }
+                                }
+                                else
+                                {
+
+                                    return true;
+                                }
+                            }
+                            else
+                            {
+
+                                return true;
+                            }
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                       return true;
+                    }
+                   
+                }
+                else
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        else if (pieza == "n")
+        {
+            if (tablero[x + 2][y - 1] == "p" || tablero[x + 2][y - 1] == "r" || tablero[x + 2][y - 1] == "b" || tablero[x + 2][y - 1] == "n" || tablero[x + 2][y - 1] == "q" || tablero[x + 2][y - 1] == "k")
+            {
+                if (tablero[x + 1][y - 2] == "p" || tablero[x + 1][y - 2] == "r" || tablero[x + 1][y - 2] == "b" || tablero[x + 1][y - 2] == "n" || tablero[x + 1][y - 2] == "q" || tablero[x + 1][y - 2] == "k")
+                {
+                    if (tablero[x - 1][y - 2] == "p" || tablero[x - 1][y - 2] == "r" || tablero[x - 1][y - 2] == "b" || tablero[x - 1][y - 2] == "n" || tablero[x - 1][y - 2] == "q" || tablero[x - 1][y - 2] == "k")
+                    {
+                        if (tablero[x - 2][y - 1] == "p" || tablero[x - 2][y - 1] == "r" || tablero[x - 2][y - 1] == "b" || tablero[x - 2][y - 1] == "n" || tablero[x - 2][y - 1] == "q" || tablero[x - 2][y - 1] == "k")
+                        {
+                            if (tablero[x - 2][y + 1] == "p" || tablero[x - 2][y + 1] == "r" || tablero[x - 2][y + 1] == "b" || tablero[x - 2][y + 1] == "n" || tablero[x - 2][y + 1] == "q" || tablero[x - 2][y + 1] == "k")
+                            {
+                                if (tablero[x - 1][y + 2] == "p" || tablero[x - 1][y + 2] == "r" || tablero[x - 1][y + 2] == "b" || tablero[x - 1][y + 2] == "n" || tablero[x - 1][y + 2] == "q" || tablero[x - 1][y + 2] == "k")
+                                {
+                                    if (tablero[x + 1][y + 2] == "p" || tablero[x + 1][y + 2] == "r" || tablero[x + 1][y + 2] == "b" || tablero[x + 1][y + 2] == "n" || tablero[x + 1][y + 2] == "q" || tablero[x + 1][y + 2] == "k")
+                                    {
+                                        if (tablero[x + 2][y + 1] == "p" || tablero[x + 2][y + 1] == "r" || tablero[x + 2][y + 1] == "b" || tablero[x + 2][y + 1] == "n" || tablero[x + 2][y + 1] == "q" || tablero[x + 2][y + 1] == "k")
+                                        {
+                                            return false;
+                                        }
+                                        else
+                                        {
+                                           return true;
+                                        }
+                                    }
+                                    else
+                                    {
+                                      return true;
+                                       
+                                    }
+                                }
+                                else
+                                {
+                                   return true;
+                                }
+                            }
+                            else
+                            {
+                               return true;
+                            }
+                        }
+                        else
+                        {
+                           return true;
+                        }
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                   return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
+            return false;
+        }
     }
 };
 #endif
